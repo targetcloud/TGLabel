d39a5bbdd7721e008abe5512f5cc892f055dd522
targetcloud <14185554@qq.com>
2017-03-03 18:26:40 +0800

    0.0.1

--- TGLabelDemo/TGLabelDemo/ViewController.swift
+++ TGLabelDemo/TGLabelDemo/ViewController.swift
@@ -9,10 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var testLbl: TGLabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        testLbl.text = "TGLabel https://github.com/targetcloud/TGLabel"
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,4 +23,3 @@ class ViewController: UIViewController {
 
 
 }
-
